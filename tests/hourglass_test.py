28579132b8e65ea79b79da8af38467610f4a6e40
Caio Lima <caio-augusto@live.com>
2024-07-11 09:12:12 -0300

    comparative tests on hourglass

--- tests/hourglass_test.py
+++ tests/hourglass_test.py
@@ -17,6 +17,21 @@ def hourglass_test():
     hg = HourGlass(seconds=5, visibility=True).start()
     sleep(5)
 
+def comparative_tests():
+    hg1 = HourGlass(seconds=5, visibility=True).start()
+    sleep(0.5)
+    hg2 = HourGlass(seconds=5, visibility=True).start()
+    
+    print(f"hg1 >= hg2: {hg1 >= hg2}")
+    print(f"hg1 <= hg2: {hg1 <= hg2}")
+    print(f"hg1 == hg2: {hg1 == hg2}")
+    print(f"hg1 != hg2: {hg1 != hg2}")
+    print(f"hg1 > hg2: {hg1 > hg2}")
+    print(f"hg1 < hg2: {hg1 < hg2}")
+
 if __name__ == "__main__":
     print("\nTeste 1:")
     hourglass_test()
+    sleep(1)
+    print("\nTeste 2:")
+    comparative_tests()
