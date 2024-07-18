b197bc4768add071c3aba8cf8657fe045aac6da4
Caio Lima <caio-augusto@live.com>
2024-07-18 15:23:02 -0300

    several changes on timer exhibition methods and typing (from datetime.time to timedelta; I learned that while 'datetime.time' refers to some time of day (between 12:00 AM and 11:59PM), 'timedelta' refers to an AMOUNT of time

--- src/ptymer/_timer.py
+++ src/ptymer/_timer.py
@@ -1,4 +1,4 @@
-from datetime import datetime
+from datetime import datetime, timedelta
 from contextlib import ContextDecorator
 from typing import Optional, Union, Dict, Tuple
 
@@ -19,13 +19,13 @@ class Timer(ContextDecorator):
             - Raises an error if `visibility` is not a boolean.
         """
         self.__start_time: Optional[datetime] = None
-        self.__marks: list[list[str, ]] = []
+        self.__marks: list[list[timedelta, Optional[str]]] = []
         if not isinstance(visibility, bool):
             raise TypeError("Visibility must be a boolean!")
-        else: self.__visibility: bool = visibility
+        else: self.visibility: bool = visibility
 
     def __str__(self) -> str:
-        return f"Class Timer()\nVisibility: {self.__visibility}\nActive: {self.is_active}\nStart time: {str(self.__start_time)}\nTime since start: {str(self.current_time())}\nQuantity of marks: {len(self.__marks)}\n"
+        return f"Class Timer()\nVisibility: {self.visibility}\nActive: {self.is_active()}\nStart time: {str(self.__start_time)}\nTime since start: {str(self.current_time())}\nQuantity of marks: {len(self.__marks)}\n"
     
     def __enter__(self) -> "Timer":
         """
@@ -59,10 +59,10 @@ class Timer(ContextDecorator):
         Notes:
             - If no exception occurred (`exc_type` is `None`), the timer is stopped.
         """
-        if not self.is_active():
-            raise AttributeError(f"There is no timer executing!")
-        elif exc_type:
+        if exc_type:
             raise exc_type(exc_value)
+        elif not self.is_active():
+            raise AttributeError(f"There is no timer executing!")
         else:
             self.stop()
         
@@ -95,62 +95,33 @@ class Timer(ContextDecorator):
         if isinstance(other, Timer):
             return self.__start_time >= other.__start_time
         else: raise TypeError(f"Cannot compare HourGlass with {type(other)}!")
-
-    @staticmethod
-    def _time_format(secs: Union[int, float]) -> datetime.time:
-        """
-        Convert seconds to a `datetime.time` object.
-
-        This method takes a number of seconds and converts it to a `datetime.time` object, 
-        representing the equivalent hours, minutes, and seconds.
-
-        Args:
-            secs (Union[int, float]): The number of seconds to convert.
-
-        Returns:
-            datetime.time: A `datetime.time` object representing the equivalent time.
-
-        Raises:
-            ValueError: If the provided seconds cannot be converted to a valid time.
-
-        Notes:
-            - The input `secs` can be either an integer or a float.
-            - The method prints the intermediate time string for debugging purposes.
-            - The time is formatted to include hours, minutes, and seconds with two decimal places for seconds.
-        """
-        mins, secs = divmod(secs, 60)
-        hours, mins = divmod(mins, 60)
- 
-        time_str = f"{int(hours)} {int(mins)} {float(secs):.2f}"
-        return datetime.strptime(time_str, "%H %M %S.%f").time()
         
-    def start(self) -> datetime.time:
+    def start(self) -> "Timer":
         """
         Start the timer and set the start time.
 
         Returns:
-            datetime.time: The start time of the timer as a `datetime.time` object.
+            Timer: The current instance of the `Timer` class
 
         Raises:
             RuntimeError: If the timer is already executing.
 
         Notes:
             - Sets `self.__start_time` to the current timestamp using `datetime.now()`.
-            - If `self.__visibility` is `True`, prints the formatted start time using `_time_format(0)`.
         """
         if self.is_active():
             raise RuntimeError(f"Timer already executing!")
         else:
             self.__start_time = datetime.now()
-            print(f"Starttime: {str(self._time_format(0))}") if self.__visibility else None
+            print(f"Starting timer!") if self.visibility else None
             return self
     
-    def stop(self) -> datetime.time:
+    def stop(self) -> timedelta:
         """
         Stop the timer and set the end time.
 
         Returns:
-            datetime.time: The end time of the timer as a `datetime.time` object.
+            timedelta: The end time of the timer as a `timedelta` object.
 
         Raises:
             AttributeError: If there is no timer currently executing.
@@ -158,19 +129,19 @@ class Timer(ContextDecorator):
         Notes:
             - Calculates the end time using the difference between the current time and `self.__start_time`.
             - Resets `self.__start_time` to `None` after stopping the timer.
-            - If `self.__visibility` is `True`, prints the formatted end time and lists any recorded marks.
+            - If `self.visibility` is `True`, prints the formatted end time and lists any recorded marks.
         """
         if not self.is_active():
             raise AttributeError(f"There is no timer executing!")
         else:
-            end_time = self._time_format((datetime.now() - self.__start_time).total_seconds())
+            end_time = datetime.now() - self.__start_time
             self.__start_time = None
 
-            if self.__visibility:
-                print(f"Endtime: {str(end_time)}")
+            if self.visibility:
+                print(f"Total elapsed time: {str(end_time)}")
                 if len(self.__marks) > 0:
                     print("Marks:")
-                    self.list_marks()
+                    [print(f"{x+1}: {mark[0]}" + "\t" + f"{mark[1]}") for x, mark in enumerate(self.__marks)]
 
             return end_time
         
@@ -184,23 +155,23 @@ class Timer(ContextDecorator):
         Notes:
             - Updates `self.__start_time` to the current timestamp using `datetime.now()`.
             - Resets `self.__marks` to an empty list.
-            - If `self.__visibility` is `True`, prints the timestamp of the restart.
+            - If `self.visibility` is `True`, prints the restart message.
         """
         if not self.is_active():
-            raise RuntimeError(f"There is no timer running!")
+            raise RuntimeError(f"There is no timer executing!")
         else:
             now = datetime.now()
-            if self.__visibility:
+            if self.visibility:
                 print(f"Restarting timer!")
             self.__start_time = now
             self.__marks = []
     
-    def current_time(self) -> datetime.time:
+    def current_time(self) -> timedelta:
         """
         Return the current elapsed time of the timer from the start time.
 
         Returns:
-            datetime.time: The current elapsed time in `HH:MM:SS.ms` format.
+            timedelta: The current elapsed time in `HH:MM:SS.ms` format.
 
         Raises:
             AttributeError: If there is no timer currently executing.
@@ -212,7 +183,7 @@ class Timer(ContextDecorator):
         if not self.is_active():
             raise AttributeError(f"There is no timer executing!")
         else:
-            return self._time_format((datetime.now() - self.__start_time).total_seconds())
+            return datetime.now() - self.__start_time
     
     def mark(self, observ: Optional[str] = None) -> None:
         """
@@ -226,46 +197,33 @@ class Timer(ContextDecorator):
 
         Notes:
             - Adds a new mark to `self.__marks` consisting of the current time and the observation.
-            - If `self.__visibility` is `True`, prints the mark number, current time, and observation.
+            - If `self.visibility` is `True`, prints the mark number, current time, and observation.
         """
         if not self.is_active():
             raise AttributeError(f"There is no timer executing!")
         else:
-            if self.__visibility:
-                print(f"Mark {len(self.__marks)+1}: {str(self.current_time())}" + "\t" + f"{(observ) if observ else ''}")
+            if self.visibility:
+                print(f"Mark {len(self.__marks)+1}! \nTime since the beginning: \t{str(self.current_time())}" + (f"\nTime since previous mark: \t{self.current_time()-self.__marks[-1][0]}" if len(self.__marks) > 0 else "") + (f"\nObs: {observ}" if observ else ""))
             self.__marks.append([self.current_time(), observ if observ else ""])
 
-    def list_marks(self) -> Dict[int, Tuple[str, datetime.time]]:
+    def list_marks(self) -> Dict[int, Tuple[str, timedelta]]:
         """
         Return a dictionary with the marks and their respective times.
 
         Returns:
-            Dict[int, Tuple[str, datetime.time]]: A dictionary where keys are mark indices 
+            Dict[int, Tuple[str, timedelta]]: A dictionary where keys are mark indices 
             and values are tuples containing the observation (if any) and the mark time.
 
         Raises:
             AttributeError: If there are no marks to show.
 
         Notes:
-            - If `self.__visibility` is `True`, prints each mark with its index, observation, and time.
             - Returns a dictionary representation of `self.__marks` where each entry contains the 
-            mark index as key and a tuple of observation (or empty string) and mark time.
+            mark index as key and a tuple of mark time and observation (or empty string).
         """
         if self.__marks == []:
             raise AttributeError(f"There are no marks to show!")
-        elif self.__visibility:
-            [print(f"{x+1}: {mark[0]}" + "\t" + f"{mark[1]}") for x, mark in enumerate(self.__marks)]
-        return {idx: (sublist[0], sublist[1]) for idx, sublist in enumerate(self.__marks)}
-    
-    def show(self) -> None:
-        """
-        Print the current elapsed time of the timer in `HH:MM:SS.ms` format.
-
-        Notes:
-            - Uses the `current_time` method to retrieve and format the current elapsed time.
-            - Prints the formatted current time with a descriptive label.
-        """
-        print(f"Current time: {str(self.current_time())}")
+        return {idx: [sublist[0], sublist[1]] for idx, sublist in enumerate(self.__marks)}
 
     def is_active(self) -> bool:
         """
